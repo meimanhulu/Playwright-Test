864d8fb6f36d1310a444d62d6053e6af3b4bf744
Meiman Ryland Hulu <94421861+meimanhulu@users.noreply.github.com>
2025-07-28 18:17:11 +0700

    push

--- src/locators/SignUpLocators.ts
+++ src/locators/SignUpLocators.ts
@@ -4,7 +4,7 @@ export class SignUpLocator {
   // Sign‑up locators
   btnStartSignup: Locator;
   inputEmail: Locator;
-  btnNext1: Locator;
+  btnNext: Locator;
   inputUsername: Locator;
   inputPassword: Locator;
   btnSignUp1: Locator;
@@ -15,15 +15,28 @@ export class SignUpLocator {
   companyName: Locator;
   companyDropDown: Locator;
   sizeOption: Locator;
+  companyIndustry: Locator;
+  industryOption: Locator;
+  fieldTimeZone: Locator;
+  btnNext1: Locator;
+  SignUpChannel: Locator;
+  emptyCheck: Locator;
+  btnNext2: Locator;
+
+
+
 
 
   constructor(page: Page) {
     
     this.btnStartSignup = page.locator('a.cf848612d.c7e1be07f');
     this.inputEmail    = page.locator('input[name="email"]');
-    this.btnNext1      = page.locator('button[name="action"]');
+    this.btnNext      = page.locator('button[name="action"]');
     this.inputUsername = page.locator('input[id="username"]');
     this.inputPassword = page.locator('input[type="password"]');
     this.btnSignUp1    = page.locator('button[name="action"]');
+    this.fieldTimeZone = page.locator('//input[contains(@data-gtm-form-interact-field-id, "0") and contains(., "")]');
+    this.btnNext1    = page.locator('//button[normalize-space(text())="Next"]');
+    this.btnNext2    = page.locator('//button[normalize-space(text())="Next"]');
   }
 }
